7de58337c54f8608f4eb988ca0664ac60b185590
Honiyad36 <shaikhoniyad36@gmail.com>
2026-07-20 21:37:10 +0530

    [905]. Sort Array by Parity.java

--- [905]. Sort Array by Parity.java
+++ [905]. Sort Array by Parity.java
@@ -1,22 +1,31 @@
 class Solution {
-    public int[] sortArrayByParity(int[] nums) {
-        int left = 0;
-        int right = nums.length -1;
-        while(left<right){
-            if(nums[left]%2==0){      
-                left++;
-            }
-            else if(nums[right]%2!=0){
-                right--;                      
-            }
-            else{
-                int temo = nums[left];           
-                nums[left]= nums[right];         
-                nums[right] = temo;                
-                left++;
-                right--;
+    public int numPermsDISequence(String s) {
+        int n = s.length();
+        int MOD = 1_000_000_007;
+        int[][] dp = new int[n + 1][n + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            char move = s.charAt(i - 1);
+            if (move == 'I') {
+                int runningSum = 0;
+                for (int j = 0; j <= i; j++) {
+                    if (j > 0) {
+                        runningSum = (runningSum + dp[i - 1][j - 1]) % MOD;
+                    }
+                    dp[i][j] = runningSum;
+                }
+            } else {
+                int runningSum = 0;
+                for (int j = i - 1; j >= 0; j--) {
+                    runningSum = (runningSum + dp[i - 1][j]) % MOD;
+                    dp[i][j] = runningSum;
+                }
             }
         }
-        return nums;
+        int ans = 0;
+        for (int j = 0; j <= n; j++) {
+            ans = (ans + dp[n][j]) % MOD;
+        }
+        return ans;
     }
 }
