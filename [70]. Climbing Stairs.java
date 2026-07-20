d3cfbd2bfff101dd47dcce109593f089d3137fc9
Honiyad36 <shaikhoniyad36@gmail.com>
2026-07-20 19:40:08 +0530

    [70]. Climbing Stairs.java

---
+++ [70]. Climbing Stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n==1||n==0||n==2){
+           return n;
+        }
+        int c = 2; int p =1;
+        for(int i=3; i<=n; i++ ){
+           int t = c;                           
+           c=c+p;         
+           p = t;      
+        }    
+        return c;
+    }
+}
